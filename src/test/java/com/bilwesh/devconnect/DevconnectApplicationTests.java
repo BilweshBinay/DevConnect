4a24cb4d7415950bffb7fd9922537c5ee15a1db4
Bilwesh <bilweshbinay1025@gmail.com>
2026-07-17 20:21:52 +0530

    Initial commit: DeveConnect backend structure & configuration

---
+++ src/test/java/com/bilwesh/devconnect/DevconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bilwesh.devconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
